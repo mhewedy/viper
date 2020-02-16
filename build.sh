ee562d12cc8feeb4006a334cc1bac494482c6c80
Muhammad Hewedy <mhewedy@gmail.com>
2020-02-16 13:00:05 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ mkdir -p "$HOME"/bin
 ln -sf "$(pwd)"/vm "$HOME"/bin/vm
 cd vmip; make; cd -
 
-printf "\n# vm autocompletion script \nif [ -f '$(pwd)/zsh-autocompletion.sh' ]; then . '$(pwd)/zsh-autocompletion.sh'; fi\n\n" >> "$HOME"/.zshrc
+printf "\n# vm autocompletion script \nif [ -f '$(pwd)/zsh-autocompletion.sh' ]; then . '$(pwd)/zsh-autocompletion.sh'; fi\n\n" >> "$HOME"/.zshrc > /dev/null
