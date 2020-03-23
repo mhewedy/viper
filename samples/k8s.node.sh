1e903ceedbdc778b533cda6070a14a1817f45fd7
Muhammad Hewedy <mhewedy@gmail.com>
2020-03-24 01:21:33 +0300

    add sample k8s cluster init files

---
+++ samples/k8s.node.sh
@@ -0,0 +1,30 @@
+swapoff -a && sed -i '/ swap / s/^/#/' /etc/fstab
+
+####
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+###
+
+sudo apt-get update -y
+
+sudo apt-get install -y docker-ce=18.06.1~ce~3-0~ubuntu kubelet=1.13.5-00 kubeadm=1.13.5-00 kubectl=1.13.5-00
+
+sudo apt-mark hold docker-ce kubelet kubeadm kubectl
+
+###
+
+echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
+
+sudo sysctl -p
+
