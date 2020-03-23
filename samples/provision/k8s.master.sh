5939db1a513a4aeeeb933688fb0216d408dab740
Muhammad Hewedy <mhewedy@gmail.com>
2020-03-24 02:02:19 +0300

    clean up

--- samples/provision/k8s.master.sh
+++ samples/provision/k8s.master.sh
@@ -1,15 +1,13 @@
+sudo hostnamectl set-hostname "$(hostname -I | awk '{print $1}')"
 swapoff -a && sed -i '/ swap / s/^/#/' /etc/fstab
 
 ####
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
 sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-
 cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
@@ -17,29 +15,21 @@ EOF
 ###
 
 sudo apt-get update -y
-
 sudo apt-get install -y docker-ce=18.06.1~ce~3-0~ubuntu kubelet=1.13.5-00 kubeadm=1.13.5-00 kubectl=1.13.5-00
-
 sudo apt-mark hold docker-ce kubelet kubeadm kubectl
 
 ###
 
 echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
-
 sudo sysctl -p
 
 ###
 
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16 &> ~/kubeadm.log
-
 mkdir -p $HOME/.kube
-
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-
 sudo chown "$(id -u):$(id -g)" $HOME/.kube/config
-
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
-
 
 #####
 
@@ -57,4 +47,3 @@ sudo yq w -i /etc/netplan/50-cloud-init.yaml network.ethernets.enp0s3.nameserver
 sudo netplan apply
 
 #####
-sudo hostnamectl set-hostname "$(hostname -I | awk '{print $1}')"
--- samples/provision/k8s.node.sh
+++ samples/provision/k8s.node.sh
@@ -1,15 +1,13 @@
+sudo hostnamectl set-hostname "$(hostname -I | awk '{print $1}')"
 swapoff -a && sed -i '/ swap / s/^/#/' /etc/fstab
 
 ####
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
 sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-
 cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
@@ -17,15 +15,12 @@ EOF
 ###
 
 sudo apt-get update -y
-
 sudo apt-get install -y docker-ce=18.06.1~ce~3-0~ubuntu kubelet=1.13.5-00 kubeadm=1.13.5-00 kubectl=1.13.5-00
-
 sudo apt-mark hold docker-ce kubelet kubeadm kubectl
 
 ###
 
 echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
-
 sudo sysctl -p
 
 #####
@@ -42,6 +37,3 @@ sudo yq w -i /etc/netplan/50-cloud-init.yaml network.ethernets.enp0s3.nameserver
 sudo yq w -i /etc/netplan/50-cloud-init.yaml network.ethernets.enp0s3.nameservers.addresses[+] 8.8.4.4
 
 sudo netplan apply
-
-#####
-sudo hostnamectl set-hostname "$(hostname -I | awk '{print $1}')"
