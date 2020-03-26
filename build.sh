013811dd7aef12a0a58294ffea1c20da5149c57c
Muhammad Hewedy <mhewedy@gmail.com>
2020-03-26 18:43:22 +0300

    allow ssh using auth key pair

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ mkdir -p "$HOME"/bin
 ln -sf "$(pwd)"/vm "$HOME"/bin/vm
 cd vmip; make; cd -
 
+mkdir -p "$HOME"/.ssh
+cp -rf keys/vm_rsa $HOME/.ssh; chmod 600 $HOME/.ssh/vm_rsa
+
 if [ -f "$HOME"/.zshrc ]; then
   printf "\n# vm autocompletion script
 if [ -f '%s/zsh-autocompletion.sh' ];then
