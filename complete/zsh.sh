42066662a4c6d742c11dddb2c013b6a799ac4a03
Muhammad Hewedy <mhewedy@gmail.com>
2020-04-03 13:56:17 +0300

    fix zsh autocompletion

--- complete/zsh.sh
+++ complete/zsh.sh
@@ -3,7 +3,7 @@
 # TODO allow multiple input vms for start, stop and rm
 
 stoppedVms() {
-  diff <(vm ls) <(vm ps) | grep '<' | cut -d " " -f2
+  diff <(vboxmanage list vms | cut -d " " -f1 | cut -d "\"" -f2) <(vboxmanage list runningvms | cut -d " " -f1 | cut -d "\"" -f2) | grep '<' | cut -d " " -f2
 }
 
 _vm() {
@@ -15,8 +15,8 @@ _vm() {
     case $words[2] in
     create) _files ;;
     start) _describe 'command' "($(stoppedVms))" ;;
-    rm) _describe 'command' "($(vm ls))" ;;
-    ssh | stop | port | cp | ip) _describe 'command' "($(vm ps))" ;;
+    rm) _describe 'command' "($(vboxmanage list vms | cut -d " " -f1 | cut -d "\"" -f2))" ;;
+    ssh | stop | port | cp | ip) _describe 'command' "($(vboxmanage list runningvms | cut -d " " -f1 | cut -d "\"" -f2))" ;;
     esac
     ;;
   esac
