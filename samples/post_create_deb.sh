179fc8558ddd8897071145ff49227c9ea09ea776
Muhammad Hewedy <mhewedy@gmail.com>
2020-04-05 00:57:36 +0300

    create post_create_deb.sh

--- samples/post_create_deb.sh
+++ samples/post_create_deb.sh
@@ -1,4 +1,4 @@
 sudo apt install -y openssh-server
-useradd -m -s /bin/bash vm_user
+sudo useradd -m -s /bin/bash vm_user
 echo "vm_user ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
 wget https://raw.githubusercontent.com/mhewedy/vm/master/keys/vm_rsa.pub -O - >>~/.ssh/authorized_keys
