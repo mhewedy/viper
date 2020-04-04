d0beafdeaa01bece57977dcf2a631380ec533224
Muhammad Hewedy <mhewedy@gmail.com>
2020-04-05 02:11:54 +0300

    create post_create_deb.sh

--- samples/post_create_deb.sh
+++ samples/post_create_deb.sh
@@ -3,4 +3,4 @@ sudo useradd -m -s /bin/bash vm_user
 echo "vm_user ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
 sudo mkdir -p /home/vm_user/.ssh &&
   wget https://raw.githubusercontent.com/mhewedy/vm/master/keys/vm_rsa.pub -O - | sudo tee -a /home/vm_user/.ssh/authorized_keys &&
-  sudo chmod 400 /home/vm_user/.ssh/authorized_keys
+  sudo chmod 400 /home/vm_user/.ssh/authorized_keys && sudo chown vm_user:vm_user /home/vm_user/.ssh/authorized_keys
