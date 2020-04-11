99f1c5dc8023aeb448e565d8e727d36d861a5557
Muhammad Hewedy <mhewedy@gmail.com>
2020-04-11 03:43:32 +0300

    rename to viper

--- samples/post_create_deb.sh
+++ samples/post_create_deb.sh
@@ -1,5 +1,6 @@
 sudo apt install -y openssh-server
-sudo useradd -m -s /bin/bash -p "$(openssl passwd -1 viper)" viper
+sudo useradd -m -s /bin/bash viper
+echo "viper:viper" | sudo chpasswd
 echo "viper ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
 sudo mkdir -p /home/viper/.ssh &&
   wget https://raw.githubusercontent.com/mhewedy/viper/master/keys/viper_rsa.pub -O - | sudo tee -a /home/viper/.ssh/authorized_keys &&
